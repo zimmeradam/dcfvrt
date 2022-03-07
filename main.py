937aa5cdea92dfe537533be64201437a4e270881
zimmeradam <63109706+zimmeradam@users.noreply.github.com>
2022-03-07 18:38:54 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from telegram_a import sendMsg
-from html_pages import Success, Failure
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 app = FastAPI()
 
 
 lista={}
 lista['1134'] = '-778541371'
 
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read(request: Request):
+    return templates.TemplateResponse("item.html",
+        {"request": request})
+
 @app.get("/{item_id}", response_class=HTMLResponse)
-async def read_item(item_id):
+async def read_item(item_id, request: Request):
     try:
         bot_chatID = lista[str(item_id)]
         sendMsg(item_id, bot_chatID)
-        return Success() #{"chat_id": bot_chatID}
+        return templates.TemplateResponse("success.html",
+        {"request": request})
     except:
-        return Failure()
+        return templates.TemplateResponse("failure.html",
+        {"request": request})
 
     
